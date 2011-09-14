ff09f8615ee5be58c14ce6319b957ba23982eb0d
Rudolf Schmidt <me@rudionrails.com>
2011-09-14 13:03:19 +0200

    updated bundle command

--- lib/capistrano/recipes/deploy/strategy/copy_with_bundler.rb
+++ lib/capistrano/recipes/deploy/strategy/copy_with_bundler.rb
@@ -97,14 +97,14 @@ module Capistrano
           bundle_gemfile = configuration[:bundle_gemfile]     || "Gemfile"
           bundle_without = [ *(configuration[:bundle_without] || [:development, :test]) ].compact
 
-          args = ["--gemfile=#{File.join(destination, bundle_gemfile)}"]
-          args << "--path=#{bundle_dir}" unless bundle_dir.to_s.empty?
+          args = ["--gemfile #{File.join(destination, bundle_gemfile)}"]
+          args << "--path #{bundle_dir}" unless bundle_dir.to_s.empty?
           args << bundle_flags.to_s
           args << "--without #{bundle_without.join(" ")}" unless bundle_without.empty?
 
-          #Dir.chdir( destination ) do
-            system( "cd #{destination} && #{bundle_cmd} install #{args.join(' ')}" )
-          #end
+          Dir.chdir( destination ) do
+            system( "#{bundle_cmd} install #{args.join(' ')}" )
+          end
         end
         
       end
